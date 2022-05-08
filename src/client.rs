2791e82de0865844a21e8fb1474f3f03d5dc84ca
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-05-09 02:22:44 +0700

    client send hello req

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,15 @@
-fn main() {
-  println!("Hello, world!");
+use belajar_rust_grpc::api::hello_gen::{
+  None,
+  hello_client::HelloClient,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = HelloClient::connect("http://[::1]:50051").await?;
+    let req = tonic::Request::new(None{});
+    let res = client.say(req).await?;
+
+    println!("RESPONSE: {}", res.into_inner().message);
+
+    Ok(())
 }
